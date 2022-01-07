555507f540485957afe1a85684520270df43e487
KaNN <lasseg123@hotmail.com>
2022-01-07 11:51:32 +0100

    Player position added to class

--- Game/Main/Game/Player.java
+++ Game/Main/Game/Player.java
@@ -3,8 +3,12 @@ public class Player {
     int balance;
     String name;
 
-    Player(int balance, String name){
+    int position;
+    Field[] ownedFields;
+
+    Player(int balance, String name, int position){
         this.balance = balance;
         this.name = name;
+        this.position = position;
     }
 }
