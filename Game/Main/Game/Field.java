06fe458911a9c2857b8e210cf7b02cca42b81a05
Sebastian <Sig.goldbach@gmail.com>
2022-01-07 10:49:15 +0100

    Basic parent field class

---
+++ Game/Main/Game/Field.java
@@ -0,0 +1,21 @@
+public class Field {
+    final String name;
+
+    public Field(String name, int position) {
+        this.name = name;
+    }
+
+    public Field(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void landOn(){
+
+    }
+}
+
+
