c52f05c7290e91c0686b9344f0e8e0babad7ec83
KaNN <lasseg123@hotmail.com>
2022-01-07 11:46:33 +0100

    Settings class and prototype player

---
+++ Game/Main/Game/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+
+    int balance;
+    String name;
+
+    Player(int balance, String name){
+        this.balance = balance;
+        this.name = name;
+    }
+}
---
+++ Game/Main/Game/Settings.java
@@ -0,0 +1,14 @@
+public final class Settings {
+
+    private Settings(){
+
+    }
+
+    // Game Rules
+    public static final int STARTING_MONEY = 30000;
+
+    // Players
+    public static final int MAX_PLAYERS = 6;
+    public static final int MIN_PLAYERS = 3;
+
+}
