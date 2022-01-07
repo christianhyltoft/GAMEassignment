524ebf3fb45c5ba8edc2344122983f349a0e7626
Christian <christian@hyltoft.net>
2022-01-07 12:11:43 +0100

    created JailField

--- Game/Main/Game/Field.java
+++ Game/Main/Game/Field.java
@@ -21,6 +21,7 @@ public abstract class Field {
 
     public void landOn() {
 
+
     }
 
 
---
+++ Game/Main/Game/JailField.java
@@ -0,0 +1,10 @@
+public class JailField extends Field{
+    public String name;
+    public String message;
+
+    public JailField(String name, String message){
+        this.message = message;
+        this.name = name;
+    }
+
+}
