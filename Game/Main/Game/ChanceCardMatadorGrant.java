1cf471ef3ddfc626b9aa8e22e89d85b79843e01c
Valdemar <v.nielsen@live.dk>
2022-01-08 14:22:08 +0100

    Updated chance card classes

---
+++ Game/Main/Game/ChanceCardMatadorGrant.java
@@ -0,0 +1,7 @@
+public class ChanceCardMatadorGrant extends ChanceCard{
+
+    public ChanceCardMatadorGrant(int chanceCardAmount, String chanceCardText, int chanceCardID) {
+        super(chanceCardAmount, chanceCardText, chanceCardID);
+
+    }
+}
