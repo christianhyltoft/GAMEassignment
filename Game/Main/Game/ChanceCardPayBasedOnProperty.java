04a020039edcc497ee32e8955698b939fb34df2d
Valdemar <v.nielsen@live.dk>
2022-01-08 14:14:20 +0100

    Updated chance card classes

--- Game/Main/Game/ChanceCardPayBasedOnProperty.java
+++ Game/Main/Game/ChanceCardPayBasedOnProperty.java
@@ -1,2 +1,15 @@
-public class ChanceCardPayBasedOnProperty {
+public class ChanceCardPayBasedOnProperty extends ChanceCard {
+    private int houseAmount;
+    private int hotelAmount;
+
+    public ChanceCardPayBasedOnProperty(int chanceCardAmount, String chanceCardText, int chanceCardID) {
+        super(chanceCardAmount, chanceCardText, chanceCardID);
+        this.houseAmount = houseAmount;
+        this.hotelAmount = hotelAmount;
+    }
+   /* public void DrawCard(Player myPlayer){
+        myPlayer.changeBalance(houseAmount*husprisen + hotelAmount*hotelprisen);
+        }
+         !SKAL RETTES EFTER! */
 }
+
