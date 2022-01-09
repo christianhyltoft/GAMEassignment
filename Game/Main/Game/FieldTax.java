bd94c5adaf9edd38c9e4a657cf91f66b825f7057
Sebastian <Sig.goldbach@gmail.com>
2022-01-09 13:02:27 +0100

    Unit test a tax field

--- Game/Main/Game/FieldTax.java
+++ Game/Main/Game/FieldTax.java
@@ -1,12 +1,24 @@
 public class FieldTax extends Field{
+    private final int taxAmount;
+    private final int taxdecimal;
 
 
-    public FieldTax(String name, String message){
+    public FieldTax(String name, String message, int taxAmount, int taxdecimal){
         super(name, message);
-
+        this.taxAmount = taxAmount;
+        this.taxdecimal=taxdecimal;
     }
     @Override
     public void landOn(Player player){
         System.out.println("You landed on the field "+ name+"." + message);
+
+        if ((player.getBalance()*taxdecimal)/100>taxAmount){
+            player.changeBalance((-player.getBalance()*taxdecimal)/100);
+        }
+        else {
+            player.changeBalance(-taxAmount);
+        }
+
+
     }
 }
