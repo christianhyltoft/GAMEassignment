99a80545b8f982aae73016bb32ae54d0df5d2c1a
Sebastian Ellegaard <sebynoah@gmail.com>
2022-01-09 12:42:40 +0100

    Small corrections to ChanceCard classes

--- Game/Main/Game/ChanceCardMove.java
+++ Game/Main/Game/ChanceCardMove.java
@@ -4,6 +4,8 @@ private Field[] myFields;
         super(chanceCardAmount, chanceCardText, chanceCardID);
         this.myFields = myFields;
     }
+
+    @Override
     public void DrawCard(Player player){
         String[] fieldname = chanceCardText.split("\"",2);
         // move to non-ferry property.
