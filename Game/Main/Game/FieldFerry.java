d1d85689348e9c5758d2339360aa90751cb141a7
Sebastian Ellegaard <sebynoah@gmail.com>
2022-01-09 13:13:48 +0100

    Implemented a method into FieldFerry

--- Game/Main/Game/FieldFerry.java
+++ Game/Main/Game/FieldFerry.java
@@ -14,10 +14,29 @@ public class FieldFerry extends FieldPurchaseAble {
     public void setRent(int rent) {
         this.rent = rent;
     }
+
     @Override
     public void landOn(Player player){
         super.landOn(player);
-        player.changeBalance(rent);
+        if (owner == null) {
+            String yesno = "";
+            //GUI skal spørge om man vil købe grunden eller ej
+
+            if (yesno.equals("yes")) {
+                setOwner(player);
+                player.changeBalance(-buyprice);
+            }
+
+        } else {
+
+            if (player == owner) {
+                //Udskriv message+messageowned til GUI
+            }
+            else {
+                owner.changeBalance(rent);
+                player.changeBalance(-rent);
+            }
+        }
 
     }
 }
