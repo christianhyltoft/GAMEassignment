51001c895c5f1c91fd1cdc1f1ddeafbb56de00c3
Sebastian Ellegaard <sebynoah@gmail.com>
2022-01-11 15:55:14 +0100

    A little work on FieldBeverage

--- Game/Main/Game/FieldBeverage.java
+++ Game/Main/Game/FieldBeverage.java
@@ -1,3 +1,6 @@
+import gui_fields.GUI_Brewery;
+import gui_fields.GUI_Street;
+
 public class FieldBeverage extends FieldPurchaseAble {
     private int rollAmount;
     private int ownedAmount;
@@ -11,23 +14,32 @@ public class FieldBeverage extends FieldPurchaseAble {
     public void landOn(Player player, GUIController gui) {
         super.landOn(player, gui);
 
-        if (owner == null) {
-            String yesno = "";
+        GUI_Brewery ownable = (GUI_Brewery) gui.getMyGUI().getFields()[player.getPosition()];
+
+       /* if (owner == null) {
             //GUI skal spørge om man vil købe grunden eller ej
-
-            if (yesno.equals("yes")) {
+            String buy = gui.getMyGUI().getUserButtonPressed("Do you want to buy this field for: " + this.buyPrice, "yes", "no");
+            if (buy.equals("yes")) {
                 setOwner(player);
-                player.changeBalance(-buyPrice);
+                ownable.setOwnerName(player.getName());
+                player.changeBalance(-this.buyPrice);
+                gui.getMyGUI().showMessage("You now own this field");
+                ownable.setRent("The rent is: " + this.rent);
+                ownable.setBorder(gui.getMyPlayers()[player.getNumber()].getPrimaryColor());
+                gui.getMyPlayers()[player.getNumber()].setBalance(player.getBalance());
             }
-
         } else {
-
             if (player == owner) {
-                //Udskriv message+messageowned til GUI
+                gui.getMyGUI().showMessage("You own this field so nothing happens");
             } else {
-
+                int rentNow = currentRent();
+                owner.changeBalance(rentNow);
+                player.changeBalance(-rentNow);
+                gui.getMyGUI().showMessage(this.owner.getName() + " owns this field, you now owe him " + this.currentRent());
+                gui.getMyPlayers()[player.getNumber()].setBalance(player.getBalance());
+                gui.getMyPlayers()[this.owner.getNumber()].setBalance(this.owner.getBalance());
             }
-        }
+        }*/
 
     }
     @Override
