95f3f7a76251e9e699bd3f9aca88fe91c12c561f
KaNN <lasseg123@hotmail.com>
2022-01-11 13:46:45 +0100

    Variable name changes

--- Game/Main/Game/FieldTax.java
+++ Game/Main/Game/FieldTax.java
@@ -1,26 +1,24 @@
 public class FieldTax extends Field{
 
     private final int taxAmount;
-    private final int taxdecimal;
+    private final int taxDecimal;
 
-    public FieldTax(String name, String FieldType, int taxAmount, int taxdecimal){
+    public FieldTax(String name, String FieldType, int taxAmount, int taxDecimal){
         super(name, FieldType);
         this.taxAmount = taxAmount;
-        this.taxdecimal=taxdecimal;
+        this.taxDecimal = taxDecimal;
     }
+    
     @Override
     public void landOn(Player player, GUIController gui){
         gui.getMyGUI().showMessage("You landed on the field " + name + "." + Fieldtype);
 
-        if ((player.getBalance() * taxdecimal) / 100 > taxAmount){
-            player.changeBalance((-player.getBalance() * taxdecimal) / 100);
+        if ((player.getBalance() * taxDecimal) / 100 > taxAmount){
+            player.changeBalance((-player.getBalance() * taxDecimal) / 100);
         }
-        else {
-            player.changeBalance(-taxAmount);
-        }
-
-
+        else { player.changeBalance(-taxAmount); }
     }
+
     @Override
     public String toString(){
         return this.name+" If you land on this field, you will feel the wrath of the danish tax authorities!";
