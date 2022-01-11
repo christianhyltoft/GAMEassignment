86f9e93687d7fb494786705ba7c175a7c5bc6e27
Sebastian <Sig.goldbach@gmail.com>
2022-01-11 15:03:24 +0100

    Spilleren får nu en ekstra tur hvis han har slået to ens

--- Game/test/javaTest/PositiveTSTField.java
+++ Game/test/javaTest/PositiveTSTField.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositiveTSTField {
-    FieldDeed deedtest = new FieldDeed("Road1", "you landed on road 1", 1000, 200, 50,100,150,200,500,600,20);
+    FieldDeed deedtest = new FieldDeed("Road1", "you landed on road 1",null, 1000, 200, 50,100,150,200,500,600,20);
+
+    public PositiveTSTField() throws IOException {
+    }
 
     @Test
     public void housetest(){
