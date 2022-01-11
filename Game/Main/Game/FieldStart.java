82bfe98fb5c3ac70adce9d69f1b176fffbf92721
KaNN <lasseg123@hotmail.com>
2022-01-11 13:48:10 +0100

    FieldStart fixes

--- Game/Main/Game/FieldStart.java
+++ Game/Main/Game/FieldStart.java
@@ -1,13 +1,14 @@
 public class FieldStart extends Field{
 
-
     public FieldStart(String name, String FieldType){
         super(name, FieldType);
     }
+
     @Override
     public void landOn(Player player, GUIController gui){
         System.out.println("You landed on the field "+ name+"." + Fieldtype);
     }
+
     public String toString(){
         return "This is the "+this.name + " and it all starts here.";
     }
