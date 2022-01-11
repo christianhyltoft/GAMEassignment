da1d2500219ba50424852493f1db698963b67450
KaNN <lasseg123@hotmail.com>
2022-01-11 13:20:19 +0100

    Refixed ChanceCardMove

--- Game/Main/Game/ChanceCardMove.java
+++ Game/Main/Game/ChanceCardMove.java
@@ -1,11 +1,13 @@
 public class ChanceCardMove extends ChanceCard{
 
-    public ChanceCardMove(String chanceCardText) {
+    private Field[] myFields;
+
+    public ChanceCardMove(String chanceCardText, Field[] myFields) {
         super(chanceCardText);
     }
 
     @Override
-    public void DrawCard(Player player, GUIController GUI, Field[] myFields){
+    public void DrawCard(Player player, GUIController GUI){
         String[] fieldname = chanceCardText.split("\"",2);
         // move to non-ferry property.
         for (int i = 0; i < myFields.length; i++) {
