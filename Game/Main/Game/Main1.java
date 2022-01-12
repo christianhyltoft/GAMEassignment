f4ea288884988e302e84e5c867b4a576efb25fcf
Sebastian Ellegaard <sebynoah@gmail.com>
2022-01-12 18:10:55 +0100

    Small change in main

--- Game/Main/Game/Main1.java
+++ Game/Main/Game/Main1.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main1 {
     public static void main(String[] args) throws IOException {
         Gamehandler game = new Gamehandler();
-        game.playGame();
+        game.Playgame();
     }
 
 
