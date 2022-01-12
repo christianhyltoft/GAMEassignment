e19652bb4d147b2ab67c88cfc8edbe80b17ee4ed
KaNN <lasseg123@hotmail.com>
2022-01-12 11:12:29 +0100

    Move to nearest ferry implemented

---
+++ Game/Main/Game/ChanceCardFerry.java
@@ -0,0 +1,28 @@
+public class ChanceCardFerry extends ChanceCard {
+
+    private Field[] myFields;
+
+    public ChanceCardFerry(String chanceCardText, Field[] myFields, ChanceCardDeck parent) {
+        super(chanceCardText, parent);
+        this.myFields = myFields;
+    }
+
+    @Override
+    public void DrawCard(Player player, GUIController GUI) {
+        // move to ferry property.
+        for (int i = 0; i < myFields.length / 2; i++) {
+            if (myFields[player.getPosition() + i].getFieldtype().equals("Ferry")){
+                player.setPosition(player.getPosition() + i);
+                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition() + i]);
+                parent.getParent().getBoardAr()[player.getPosition() + i].landOn(player, GUI);
+                break;
+            }
+            else if (myFields[player.getPosition() - i].getFieldtype().equals("Ferry")){
+                player.setPosition(player.getPosition() - i);
+                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition() - i]);
+                parent.getParent().getBoardAr()[player.getPosition() - i].landOn(player, GUI);
+                break;
+            }
+        }
+    }
+}
