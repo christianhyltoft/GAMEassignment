2e320037fdb1937bc04d47493c44c1b5e66b0669
KaNN <lasseg123@hotmail.com>
2022-01-12 11:44:07 +0100

    passing this on board creation

--- Game/test/javaTest/BoardgenerateTest.java
+++ Game/test/javaTest/BoardgenerateTest.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class BoardgenerateTest {
     @Test
     public static void boardtest() throws IOException {
-        Board myBoard = new Board(this);
+        Board myBoard = new Board(null);
     }
 
 
