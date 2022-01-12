385bd06a0f487ae3dd7f7b9644f63c2494912816
KaNN <lasseg123@hotmail.com>
2022-01-12 13:35:50 +0100

    fixed a test that got broken by a new parameter

--- Game/test/javaTest/PositiveTSTField.java
+++ Game/test/javaTest/PositiveTSTField.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PositiveTSTField {
-    FieldDeed deedtest = new FieldDeed("Road1", "you landed on road 1",null, 1000, 200, 50,100,150,200,500,600,20);
+    FieldDeed deedtest = new FieldDeed("Road1", "you landed on road 1",null, 1000, 200, 50,100,150,200,500,600,20, null);
 
     public PositiveTSTField() throws IOException {
     }
