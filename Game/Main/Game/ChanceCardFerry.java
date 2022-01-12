1d99f4125aa5e0d2b92712d452fd3b9e8eda002a
KaNN <lasseg123@hotmail.com>
2022-01-12 14:21:13 +0100

    ChanceCardFerry FIXXXXXXXXXX

--- Game/Main/Game/ChanceCardFerry.java
+++ Game/Main/Game/ChanceCardFerry.java
@@ -14,13 +14,13 @@ public class ChanceCardFerry extends ChanceCard {
         for (int i = 0; i < myFields.length / 2; i++) {
             if (myFields[player.getPosition() + i].getFieldtype().equals("Ferry")){
                 player.setPosition(player.getPosition() + i);
-                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition() + i]);
+                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition()]);
                 parent.getParent().getBoardAr()[player.getPosition()].landOn(player, GUI);
                 break;
             }
             else if (myFields[player.getPosition() - i].getFieldtype().equals("Ferry")){
                 player.setPosition(player.getPosition() - i);
-                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition() - i]);
+                GUI.getMyPlayers()[player.getNumber()].getCar().setPosition(GUI.getMyGUI().getFields()[player.getPosition()]);
                 parent.getParent().getBoardAr()[player.getPosition()].landOn(player, GUI);
                 break;
             }
