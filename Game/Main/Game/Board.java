a8eb1d29b9ecf60b4df6c8509367167bedef44a1
Christian <christian@hyltoft.net>
2022-01-12 10:20:05 +0100

    spacing

--- Game/Main/Game/Board.java
+++ Game/Main/Game/Board.java
@@ -18,7 +18,7 @@ public class Board {
         myDeck = new ChanceCardDeck(boardAr);
     }
 
-   private Field makeField(int i, String[] generator) {
+    private Field makeField(int i, String[] generator) {
         String[] generate1field = generator[i].split("-");
         switch (generate1field[0]) {
             case "Start":
@@ -28,17 +28,17 @@ public class Board {
             case "Jail":
                 return new FieldJail(generate1field[1], "Jail", this);
             case "Parking":
-                return new FieldParking(generate1field[1],"Parking", this);
+                return new FieldParking(generate1field[1], "Parking", this);
             case "MoveToJail":
                 return new FieldGoToJail(generate1field[1], "MoveToJail", this);
             case "Tax":
-                return new FieldTax(generate1field[1],"Tax", this, Integer.parseInt(generate1field[2]),Integer.parseInt(generate1field[3]));
+                return new FieldTax(generate1field[1], "Tax", this, Integer.parseInt(generate1field[2]), Integer.parseInt(generate1field[3]));
             case "Property":
-                return new FieldDeed(generate1field[1], "Property",this, Integer.parseInt(generate1field[2]), Integer.parseInt(generate1field[3]), Integer.parseInt(generate1field[4]), Integer.parseInt(generate1field[5]), Integer.parseInt(generate1field[6]), Integer.parseInt(generate1field[7]), Integer.parseInt(generate1field[8]), Integer.parseInt(generate1field[9]), Integer.parseInt(generate1field[10]));
+                return new FieldDeed(generate1field[1], "Property", this, Integer.parseInt(generate1field[2]), Integer.parseInt(generate1field[3]), Integer.parseInt(generate1field[4]), Integer.parseInt(generate1field[5]), Integer.parseInt(generate1field[6]), Integer.parseInt(generate1field[7]), Integer.parseInt(generate1field[8]), Integer.parseInt(generate1field[9]), Integer.parseInt(generate1field[10]));
             case "Ferry":
-                return new FieldFerry(generate1field[1],"Ferry",this, Integer.parseInt(generate1field[2]),Integer.parseInt(generate1field[3]),Integer.parseInt(generate1field[4]));
+                return new FieldFerry(generate1field[1], "Ferry", this, Integer.parseInt(generate1field[2]), Integer.parseInt(generate1field[3]), Integer.parseInt(generate1field[4]));
             case "Beverage":
-                return new FieldBeverage(generate1field[1],"Beverage",this, Integer.parseInt(generate1field[2]),Integer.parseInt(generate1field[3]));
+                return new FieldBeverage(generate1field[1], "Beverage", this, Integer.parseInt(generate1field[2]), Integer.parseInt(generate1field[3]));
 
 
         }
